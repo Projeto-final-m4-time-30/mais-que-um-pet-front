dbd2bdc417409957654307a915d136d9ba6d2679
joaolucasgozzi <lukasgozzi@gmail.com>
2022-11-09 17:32:39 -0400

    feat: editPer v1

--- src/components/ModalPet/index.jsx
+++ src/components/ModalPet/index.jsx
@@ -21,19 +21,18 @@ const customStyles = {
 };
 
 const ModalPet = () => {
-  // const [modalIsOpen, setIsOpen] = useState(false);
+  const [editPage, setEditPage] = useState(false);
 
-  // function openModal() {
-  //   setIsOpen(true);
-  // }
-
-  // function closeModal() {
-  //   setIsOpen(false);
-  // }
+  function handleEditPageOpen() {
+    setEditPage(true);
+  }
+  function handleEditPageClose() {
+    setEditPage(false);
+  }
 
   const { modalPetIsOpen, handleModalPetClose, modalPetOverview } =
     useContext(petContext);
-  const { owner } = useContext(userContext);
+  const { owner, user } = useContext(userContext);
 
   console.log(owner);
 
@@ -50,48 +49,64 @@ const ModalPet = () => {
           <button className="button-close" onClick={handleModalPetClose}>
             <AiFillCloseCircle />
           </button>
-          <button className="button-edit">
-            <AiTwotoneEdit />
-          </button>
-          <figure>
-            {modalPetOverview?.info_pet?.pet_image ? (
-              <img src={modalPetOverview?.info_pet?.pet_image} alt="" />
-            ) : (
-              <img
-                src="https://s2.glbimg.com/uBZ1jg_a6wHH5otEeT6CSKiCBjU=/e.glbimg.com/og/ed/f/original/2022/04/20/teste-descubra-qual-pet-mais-combina-com-voce-vida-de-bicho.png"
-                alt="Foto do pet"
-              />
-            )}
-          </figure>
-          <div className="modal-info-pet">
-            <h2>{modalPetOverview.name}</h2>
-            <div>
-              <span>{modalPetOverview.info_pet?.species}</span>
-              <span> {modalPetOverview.info_pet?.size}</span>
-            </div>
+          {modalPetOverview.user_register === user.id ? (
+            <button
+              className="button-edit"
+              onClick={() => setEditPage(!editPage)}
+            >
+              <AiTwotoneEdit />
+            </button>
+          ) : (
+            <></>
+          )}
+          {editPage ? (
+            <>
+              <figure>
+                {modalPetOverview?.info_pet?.pet_image ? (
+                  <img src={modalPetOverview?.info_pet?.pet_image} alt="" />
+                ) : (
+                  <img
+                    src="https://s2.glbimg.com/uBZ1jg_a6wHH5otEeT6CSKiCBjU=/e.glbimg.com/og/ed/f/original/2022/04/20/teste-descubra-qual-pet-mais-combina-com-voce-vida-de-bicho.png"
+                    alt="Foto do pet"
+                  />
+                )}
+              </figure>
+              <div className="modal-info-pet">
+                <h2>{modalPetOverview.name}</h2>
+                <div>
+                  <span>{modalPetOverview.info_pet?.species}</span>
+                  <span> {modalPetOverview.info_pet?.size}</span>
+                </div>
 
-            <span>{modalPetOverview.age}</span>
-            <span>{modalPetOverview.gender}</span>
-            {modalPetOverview.info_pet?.description ? (
-              <p>{modalPetOverview.info_pet.description}</p>
-            ) : (
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Mollitia earum dolore recusandae quia. Aliquam dolores,
-                necessitatibusipsa deleniti sed impedit, natus facere quia nam
-                quasi, voluptatibus tenetur fugit non?
-              </p>
-            )}
+                <span>{modalPetOverview.age}</span>
+                <span>{modalPetOverview.gender}</span>
+                {modalPetOverview.info_pet?.description ? (
+                  <p>{modalPetOverview.info_pet.description}</p>
+                ) : (
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia earum dolore recusandae quia. Aliquam dolores,
+                    necessitatibusipsa deleniti sed impedit, natus facere quia
+                    nam quasi, voluptatibus tenetur fugit non?
+                  </p>
+                )}
 
-            <div className="contact">
-              <p>{owner?.contact?.phone}</p>
-              <p>{owner?.email}</p>
-              <p>{owner.contact?.secondary_email}</p>
-              <p>{owner.contact?.whatsapp}</p>
-            </div>
-          </div>
-
-          <div></div>
+                <div className="contact">
+                  <p>{owner?.contact?.phone}</p>
+                  <p>{owner?.email}</p>
+                  <p>{owner.contact?.secondary_email}</p>
+                  <p>{owner.contact?.whatsapp}</p>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <form>
+                {/* description, name, pet_image, size, vaccine, age  */}
+                <input type="text" placeholder="Name" />
+              </form>
+            </>
+          )}
         </ModalContainer>
       </ReactModal>
     </>
