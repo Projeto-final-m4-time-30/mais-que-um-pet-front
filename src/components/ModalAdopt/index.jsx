a8f718b79a38f6956fce75c99adfdc50a766f3c9
Makson <maksonsantoss4@gmail.com>
2022-11-09 16:17:06 -0300

    feat: funcionalidade de adotar pet

--- src/components/ModalAdopt/index.jsx
+++ src/components/ModalAdopt/index.jsx
@@ -1,11 +1,73 @@
+import { useContext, useEffect, useState } from "react";
 import ReactModal from "react-modal";
+import { ModalContainerAdopt } from "./style";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { petContext } from "../../context/petContext";
+import { Api } from "../../services";
+import { toast } from "react-toastify";
+
+const customStyles = {
+  content: {
+    width: "700px",
+    maxWidth: "85%",
+    height: "90%",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const urlImageDefault =
+  "https://s2.glbimg.com/uBZ1jg_a6wHH5otEeT6CSKiCBjU=/e.glbimg.com/og/ed/f/original/2022/04/20/teste-descubra-qual-pet-mais-combina-com-voce-vida-de-bicho.png";
 
 const ModalAdopt = () => {
+  const { closeModal, openModalAdopt, pet } = useContext(petContext);
+  const [email, setEmail] = useState();
+
+  async function adoptPet() {
+    Api.patch(`/pet/adopt/${pet.id}`, { email })
+      .then((r) =>
+        toast.success("Doação feita com sucesso!", { autoClose: 2000 })
+      )
+      .catch(() => toast.error("Email não cadastrado", { autoClose: 2000 }));
+  }
+
   return (
-    <ReactModal>
-      <p>funcionou</p>
+    <ReactModal
+      isOpen={openModalAdopt}
+      // onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      style={customStyles}
+    >
+      <ModalContainerAdopt>
+        <button className="button-close" onClick={closeModal}>
+          <AiFillCloseCircle />
+        </button>
+        <figure>
+          <img
+            src={
+              pet?.info_pet.pet_image ? pet.info_pet.pet_image : urlImageDefault
+            }
+          />
+        </figure>
+
+        <h2>{pet?.name}</h2>
+        <form>
+          <label htmlFor="email">Email do donatário</label>
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type="button" onClick={adoptPet}>
+            Doar
+          </button>
+        </form>
+      </ModalContainerAdopt>
     </ReactModal>
   );
 };
-
 export default ModalAdopt;
--- src/context/petContext.jsx
+++ src/context/petContext.jsx
@@ -1,7 +1,36 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { Api } from "../services";
 
 export const petContext = createContext({});
 
 export const PetProvider = ({ children }) => {
-  return <petContext.Provider value={{}}>{children}</petContext.Provider>;
+  const [openModalAdopt, setOpenModalAdopt] = useState(false);
+  const [pets, setPets] = useState([]);
+  const [pet, setPet] = useState();
+
+  useEffect(() => {
+    Api.get("/pet/adoptable").then((res) => setPets(res.data));
+  }, []);
+
+  function openModal() {
+    setOpenModalAdopt(true);
+  }
+
+  function closeModal() {
+    setOpenModalAdopt(false);
+  }
+  return (
+    <petContext.Provider
+      value={{
+        openModal,
+        closeModal,
+        openModalAdopt,
+        pets,
+        pet,
+        setPet,
+      }}
+    >
+      {children}
+    </petContext.Provider>
+  );
 };
