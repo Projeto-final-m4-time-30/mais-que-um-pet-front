81857ab702349e2fe40aeea110456850f65e7157
Makson <maksonsantoss4@gmail.com>
2022-11-09 20:03:09 -0300

    fix: correção da criação de pets

--- src/validators/index.js
+++ src/validators/index.js
@@ -7,34 +7,16 @@ export const loginSchema = yup.object().shape({
 });
 
 export const registerSchema = yup.object().shape({
-  user_name: yup
-    .string()
-    .matches(
-      /^([a-zA-Zà-úÀ-Ú0-9]|-|_|\s)+$/,
-      "Esse campo deve conter apenas letras"
-    )
-    .required("Campo obrigatório"),
+  user_name: yup.string().required("Campo obrigatório"),
   email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
 
   password: yup.string().required("Campo obrigatório"),
-
 });
 
 export const createPetSchema = yup.object().shape({
   name: yup.string().required("Campo obrigatório"),
   age: yup.string().required("Campo obrigatório"),
   gender: yup.string().required("Campo obrigatório"),
+  is_adoptable: yup.boolean().default(() => true),
+  is_active: yup.boolean().default(() => true),
 });
-
-  password: yup
-    .string()
-    .required("Campo obrigatório"),
-
-});
-
-export const createPetSchema = yup.object().shape({
-	name: yup.string().required("Campo obrigatório"),
-	age: yup.string().required("Campo obrigatório"),
-	gender: yup.string().required("Campo obrigatório"),
-})
-
